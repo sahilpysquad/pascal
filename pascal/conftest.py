0b1c1be220dc44c204dd610d1592aa920e91f7d5
sahilkhirsaria <sahilkhirsaria.pysquad@gmail.com>
2022-10-11 18:32:39 +0530

    initial commit

---
+++ pascal/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from pascal.users.models import User
+from pascal.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
